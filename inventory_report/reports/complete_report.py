e42bd324c45bc1c129c6e4b05acf1ce39d3834e9
Renata Dias <renata_dias96@live.com>
2022-10-31 16:53:02 -0300

    feat: Gera o relatório completo
    Cria a classe CompleteReport, que herda as funções da classe SimpleReport e acrescenta a quantidade de produtos que cada empresa possui.

---
+++ inventory_report/reports/complete_report.py
@@ -0,0 +1,22 @@
+from inventory_report.reports.simple_report import SimpleReport
+
+
+class CompleteReport(SimpleReport):
+    @classmethod
+    def generate(cls, lista):
+        empresas = cls.get_empresas(lista)
+        produtos_por_empresa = {}
+        for empresa in empresas:
+            if produtos_por_empresa.__contains__(empresa):
+                pass
+            produtos_por_empresa.update({
+              empresa: empresas.count(empresa)
+            })
+        quantidade_produto = ""
+        for chave, valor in produtos_por_empresa.items():
+            quantidade_produto += f"- {chave}: {valor}\n"
+
+        return super().generate(lista) + (
+          "\nProdutos estocados por empresa:\n"
+          f"{quantidade_produto}"
+        )
