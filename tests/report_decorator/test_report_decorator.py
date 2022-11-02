b848180967f4a8fa8a1a24144c0189bb4120c99b
Renata Dias <renata_dias96@live.com>
2022-11-02 15:08:37 -0300

    test: Implemetação do teste do decorador report_decorator

--- tests/report_decorator/test_report_decorator.py
+++ tests/report_decorator/test_report_decorator.py
@@ -1,5 +1,30 @@
-# from inventory_report.reports.colored_report import ColoredReport
+from inventory_report.reports.colored_report import ColoredReport
+from inventory_report.reports.simple_report import SimpleReport
+
+product_list = [
+  {
+    "id": 1,
+    "nome_do_produto": "Cafe",
+    "nome_da_empresa": "Cafes Nature",
+    "data_de_fabricacao": "2020-07-04",
+    "data_de_validade": "2023-02-09",
+    "numero_de_serie": "FR48",
+    "instrucoes_de_armazenamento": "instrucao"
+  },
+  {
+    "id": 2,
+    "nome_do_produto": "Cafe",
+    "nome_da_empresa": "Cafes Nature",
+    "data_de_fabricacao": "2020-07-07",
+    "data_de_validade": "2023-02-09",
+    "numero_de_serie": "FR4845",
+    "instrucoes_de_armazenamento": "instrucao 2"
+  }
+]
 
 
 def test_decorar_relatorio():
-    pass  # Seu teste deve ser escrito aqui
+    colored_report = ColoredReport(SimpleReport()).generate(product_list)
+    assert "\033[32mData de fabricação mais antiga:" in colored_report
+    assert "\033[36m2020-07-04" in colored_report
+    assert "\033[31mCafes Nature" in colored_report
